20d1e594bc8bd67bc562e2df7fd0ee587376df4a
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-11 22:01:47 -0300

    Update posiciones.js

--- js/posiciones.js
+++ js/posiciones.js
@@ -1,1 +1,16 @@
-
+[
+  {
+    "fecha":"12/06/2026",
+    "local":"Canadá",
+    "visitante":"Bosnia",
+    "golesLocal":1,
+    "golesVisitante":1
+  },
+  {
+    "fecha":"12/06/2026",
+    "local":"Estados Unidos",
+    "visitante":"Paraguay",
+    "golesLocal":2,
+    "golesVisitante":1
+  }
+]
