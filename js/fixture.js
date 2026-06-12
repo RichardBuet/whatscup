112a58293a553a5c7c15d218ff70c779a44422e2
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-11 22:02:12 -0300

    Update fixture.js

--- js/fixture.js
+++ js/fixture.js
@@ -1,1 +1,32 @@
+fetch('data/partidos.json')
+.then(response => response.json())
+.then(partidos => {
 
+    const fixture = document.getElementById('fixture');
+
+    partidos.forEach(partido => {
+
+        fixture.innerHTML += `
+            <div class="partido">
+
+                <div class="fecha">
+                    ${partido.fecha}
+                </div>
+
+                <div class="resultado">
+                    <span>${partido.local}</span>
+
+                    <strong>
+                        ${partido.golesLocal}
+                        -
+                        ${partido.golesVisitante}
+                    </strong>
+
+                    <span>${partido.visitante}</span>
+                </div>
+
+            </div>
+        `;
+    });
+
+});
