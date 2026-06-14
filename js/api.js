d5880cedd4389951750a473ec490fd060d110058
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-14 19:34:59 -0300

    Refactor getWorldCupMatches function for clarity

--- js/api.js
+++ js/api.js
@@ -1,36 +1,55 @@
 async function getWorldCupMatches() {
 
-    try {
-
-        const response = await fetch(
-            `${API_BASE}/competitions/WC/matches`,
-            {
-                headers: {
-                    "X-Auth-Token": API_KEY
-                }
+    const response = await fetch(
+        `${API_BASE}/competitions/WC/matches`,
+        {
+            headers:{
+                "X-Auth-Token":API_KEY
             }
-        );
-        
-        console.log(response.status);
-        console.log(await response.clone().text());
-        
-        if(!response.ok){
-            throw new Error(`HTTP ${response.status}`);
         }
-        
-        if (!response.ok) {
-            throw new Error(`HTTP ${response.status}`);
-        }
+    );
 
-        return await response.json();
+    const data = await response.json();
 
-    } catch(error) {
+    console.log("API OK");
+    console.log(data);
 
-        console.error("Error API:", error);
-
-        return {
-            matches:[]
-        };
-    }
-
+    return data;
 }
+
+// async function getWorldCupMatches() {
+
+//     try {
+
+//         const response = await fetch(
+//             `${API_BASE}/competitions/WC/matches`,
+//             {
+//                 headers: {
+//                     "X-Auth-Token": API_KEY
+//                 }
+//             }
+//         );
+        
+//         console.log(response.status);
+//         console.log(await response.clone().text());
+        
+//         if(!response.ok){
+//             throw new Error(`HTTP ${response.status}`);
+//         }
+        
+//         if (!response.ok) {
+//             throw new Error(`HTTP ${response.status}`);
+//         }
+
+//         return await response.json();
+
+//     } catch(error) {
+
+//         console.error("Error API:", error);
+
+//         return {
+//             matches:[]
+//         };
+//     }
+
+// }
