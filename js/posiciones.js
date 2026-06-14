fcdf05c01bcd6330dd210565fd8ac5497492429e
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-14 19:49:27 -0300

    Update posiciones.js

--- js/posiciones.js
+++ js/posiciones.js
@@ -1,16 +1,39 @@
-[
-  {
-    "fecha":"12/06/2026",
-    "local":"Canadá",
-    "visitante":"Bosnia",
-    "golesLocal":1,
-    "golesVisitante":1
-  },
-  {
-    "fecha":"12/06/2026",
-    "local":"Estados Unidos",
-    "visitante":"Paraguay",
-    "golesLocal":2,
-    "golesVisitante":1
-  }
-]
+async function cargarPosiciones() {
+
+    const response = await fetch("data/posiciones.json");
+
+    const equipos = await response.json();
+
+    const tabla = document.getElementById("tablaPosiciones");
+
+    tabla.innerHTML = `
+        <tr>
+            <th>#</th>
+            <th>Equipo</th>
+            <th>PJ</th>
+            <th>PG</th>
+            <th>PE</th>
+            <th>PP</th>
+            <th>PTS</th>
+        </tr>
+    `;
+
+    equipos.forEach(equipo => {
+
+        tabla.innerHTML += `
+            <tr>
+                <td>${equipo.pos}</td>
+                <td>${equipo.pais}</td>
+                <td>${equipo.pj}</td>
+                <td>${equipo.pg}</td>
+                <td>${equipo.pe}</td>
+                <td>${equipo.pp}</td>
+                <td>${equipo.pts}</td>
+            </tr>
+        `;
+
+    });
+
+}
+
+cargarPosiciones();
