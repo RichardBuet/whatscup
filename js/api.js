0899fff8df68d909b37d53d2ca69053d7683d039
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-14 18:56:19 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,14 +1,31 @@
-export async function getPartidos() {
-    const res = await fetch('data/partidos.json');
-    return await res.json();
+async function getWorldCupMatches() {
+
+    const response = await fetch(
+        "https://api.football-data.org/v4/competitions/WC/matches",
+        {
+            headers: {
+                "X-Auth-Token": API_KEY
+            }
+        }
+    );
+
+    const data = await response.json();
+
+    return data;
 }
 
-export async function getTorneos() {
-    const res = await fetch('data/torneos.json');
-    return await res.json();
-}
 
-export async function getGoleadores() {
-    const res = await fetch('data/goleadores.json');
-    return await res.json();
-}
+// export async function getPartidos() {
+//     const res = await fetch('data/partidos.json');
+//     return await res.json();
+// }
+
+// export async function getTorneos() {
+//     const res = await fetch('data/torneos.json');
+//     return await res.json();
+// }
+
+// export async function getGoleadores() {
+//     const res = await fetch('data/goleadores.json');
+//     return await res.json();
+// }
