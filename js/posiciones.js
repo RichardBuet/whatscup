1e0a98f7a75a4b135ce6a97f0e3506d4573d0171
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-15 14:58:14 -0300

    Refactor cargarPosiciones to use posiciones.json

--- js/posiciones.js
+++ js/posiciones.js
@@ -1,153 +1,89 @@
 async function cargarPosiciones() {
 
     const response =
-        await fetch("data/partidos.json");
+        await fetch("data/posiciones.json");
 
-    const partidos =
+    const grupos =
         await response.json();
 
-    const equipos = {};
-
-    partidos.forEach(partido => {
-
-        if (!equipos[partido.local]) {
-
-            equipos[partido.local] = {
-                nombre: partido.local,
-                pj:0,
-                pg:0,
-                pe:0,
-                pp:0,
-                gf:0,
-                gc:0,
-                pts:0
-            };
-
-        }
-
-        if (!equipos[partido.visitante]) {
-
-            equipos[partido.visitante] = {
-                nombre: partido.visitante,
-                pj:0,
-                pg:0,
-                pe:0,
-                pp:0,
-                gf:0,
-                gc:0,
-                pts:0
-            };
-
-        }
-
-        const local =
-            equipos[partido.local];
-
-        const visitante =
-            equipos[partido.visitante];
-
-        local.pj++;
-        visitante.pj++;
-
-        local.gf += partido.golesLocal;
-        local.gc += partido.golesVisitante;
-
-        visitante.gf += partido.golesVisitante;
-        visitante.gc += partido.golesLocal;
-
-        if (
-            partido.golesLocal >
-            partido.golesVisitante
-        ) {
-
-            local.pg++;
-            local.pts += 3;
-
-            visitante.pp++;
-
-        }
-
-        else if (
-            partido.golesLocal <
-            partido.golesVisitante
-        ) {
-
-            visitante.pg++;
-            visitante.pts += 3;
-
-            local.pp++;
-
-        }
-
-        else {
-
-            local.pe++;
-            visitante.pe++;
-
-            local.pts++;
-            visitante.pts++;
-
-        }
-
-    });
-
-    const tabla =
-        Object.values(equipos);
-
-    tabla.sort((a,b)=>
-        b.pts - a.pts ||
-        (b.gf-b.gc) - (a.gf-a.gc)
-    );
-
-    const html =
+    const contenedor =
         document.getElementById(
             "tablaPosiciones"
         );
 
-    html.innerHTML = `
-        <tr>
-            <th>#</th>
-            <th>Equipo</th>
-            <th>PJ</th>
-            <th>PG</th>
-            <th>PE</th>
-            <th>PP</th>
-            <th>GF</th>
-            <th>GC</th>
-            <th>DG</th>
-            <th>PTS</th>
-        </tr>
-    `;
+    let html = "";
 
-    tabla.forEach((equipo,index)=>{
+    for (const grupo in grupos) {
 
-        html.innerHTML += `
-            <tr>
+        html += `
 
-                <td>${index+1}</td>
+            <h2 class="grupo-titulo">
+                Grupo ${grupo}
+            </h2>
 
-                <td>
-                    ${equipo.nombre}
-                </td>
+            <table class="tabla-grupo">
 
-                <td>${equipo.pj}</td>
-                <td>${equipo.pg}</td>
-                <td>${equipo.pe}</td>
-                <td>${equipo.pp}</td>
+                <thead>
 
-                <td>${equipo.gf}</td>
-                <td>${equipo.gc}</td>
+                    <tr>
 
-                <td>
-                    ${equipo.gf-equipo.gc}
-                </td>
+                        <th>#</th>
+                        <th>Equipo</th>
+                        <th>PJ</th>
+                        <th>PG</th>
+                        <th>PE</th>
+                        <th>PP</th>
+                        <th>GF</th>
+                        <th>GC</th>
+                        <th>PTS</th>
 
-                <td>
-                    ${equipo.pts}
-                </td>
+                    </tr>
 
-            </tr>
+                </thead>
+
+                <tbody>
+
         `;
-    });
+
+        grupos[grupo].forEach(
+            (equipo,index) => {
+
+                html += `
+
+                    <tr>
+
+                        <td>${index+1}</td>
+
+                        <td>${equipo.equipo}</td>
+
+                        <td>${equipo.pj}</td>
+                        <td>${equipo.pg}</td>
+                        <td>${equipo.pe}</td>
+                        <td>${equipo.pp}</td>
+
+                        <td>${equipo.gf}</td>
+                        <td>${equipo.gc}</td>
+
+                        <td>${equipo.pts}</td>
+
+                    </tr>
+
+                `;
+
+            }
+        );
+
+        html += `
+
+                </tbody>
+
+            </table>
+
+        `;
+    }
+
+    contenedor.innerHTML = html;
 
 }
+
+cargarPosiciones();
