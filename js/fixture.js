7eb1c88c493e7b79c2a0b13f455c35ca4bf12e34
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-15 16:00:08 -0300

    Update fixture.js

--- js/fixture.js
+++ js/fixture.js
@@ -1,44 +1,62 @@
 async function cargarPartidos() {
 
-    const fixture = document.getElementById("fixture");
+    const fixture =
+        document.getElementById(
+            "fixture"
+        );
 
-    fixture.innerHTML = "Cargando...";
+    fixture.innerHTML =
+        "Cargando...";
 
     const response =
-    await fetch("data/partidos.json");
+        await fetch(
+            "data/partidos.json"
+        );
 
-const partidos =
-    await response.json();
+    const partidos =
+        await response.json();
 
-    console.log(data);
+    console.log(partidos);
 
     fixture.innerHTML = "";
 
-    data.matches.slice(0,5).forEach(match => {
+    partidos
+        .slice(0,20)
+        .forEach(match => {
 
-        fixture.innerHTML += `
-            <div class="partido">
-                <div class="fecha">
-                    ${match.utcDate}
+            fixture.innerHTML += `
+
+                <div class="partido">
+
+                    <div class="fecha">
+                        ${match.fecha}
+                    </div>
+
+                    <div class="resultado">
+
+                        <span>
+                            ${match.local}
+                        </span>
+
+                        <strong>
+
+                            ${match.golesLocal}
+                            -
+                            ${match.golesVisitante}
+
+                        </strong>
+
+                        <span>
+                            ${match.visitante}
+                        </span>
+
+                    </div>
+
                 </div>
 
-                <div class="resultado">
+            `;
 
-                    <span>${match.homeTeam.name}</span>
-
-                    <strong>
-                        ${match.score.fullTime.home ?? "-"}
-                        -
-                        ${match.score.fullTime.away ?? "-"}
-                    </strong>
-
-                    <span>${match.awayTeam.name}</span>
-
-                </div>
-            </div>
-        `;
-
-    });
+        });
 
 }
 
