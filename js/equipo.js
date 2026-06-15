85ed7216da4a41db44c8940ab7c7b63e87380bd9
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-15 16:38:36 -0300

    Update equipo.js

--- js/equipo.js
+++ js/equipo.js
@@ -1,17 +1,123 @@
-const params =
-    new URLSearchParams(
-        window.location.search
-    );
+async function cargarEquipo() {
 
-const equipo =
-    params.get("equipo");
+    const params =
+        new URLSearchParams(
+            window.location.search
+        );
 
-document.getElementById(
-    "equipoDetalle"
-).innerHTML = `
+    const nombreEquipo =
+        params.get("equipo");
 
-    <h2>
-        ${equipo || "Sin equipo"}
-    </h2>
+    const response =
+        await fetch(
+            "data/posiciones.json"
+        );
 
-`;
+    const grupos =
+        await response.json();
+
+    let equipoEncontrado = null;
+
+    Object.keys(grupos).forEach(grupo => {
+
+        grupos[grupo].forEach(equipo => {
+
+            if (
+                equipo.equipo === nombreEquipo
+            ) {
+
+                equipoEncontrado = {
+                    ...equipo,
+                    grupo
+                };
+
+            }
+
+        });
+
+    });
+
+    const html =
+        document.getElementById(
+            "equipoDetalle"
+        );
+
+    if (!equipoEncontrado) {
+
+        html.innerHTML =
+            "Equipo no encontrado";
+
+        return;
+
+    }
+
+    html.innerHTML = `
+
+        <h1>
+            ${equipoEncontrado.equipo}
+        </h1>
+
+        <br>
+
+        <h2>
+            Grupo ${equipoEncontrado.grupo}
+        </h2>
+
+        <br>
+
+        <div class="equipo-card">
+
+            <p>
+                PJ:
+                ${equipoEncontrado.pj}
+            </p>
+
+            <p>
+                PG:
+                ${equipoEncontrado.pg}
+            </p>
+
+            <p>
+                PE:
+                ${equipoEncontrado.pe}
+            </p>
+
+            <p>
+                PP:
+                ${equipoEncontrado.pp}
+            </p>
+
+            <br>
+
+            <p>
+                GF:
+                ${equipoEncontrado.gf}
+            </p>
+
+            <p>
+                GC:
+                ${equipoEncontrado.gc}
+            </p>
+
+            <p>
+                DG:
+                ${
+                    equipoEncontrado.gf -
+                    equipoEncontrado.gc
+                }
+            </p>
+
+            <br>
+
+            <h2>
+                PTS:
+                ${equipoEncontrado.pts}
+            </h2>
+
+        </div>
+
+    `;
+
+}
+
+cargarEquipo();
