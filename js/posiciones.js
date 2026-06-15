5d5be9cfc4853a46d414f6e24f6b41340d4fe91e
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-15 20:59:57 -0300

    Add class to table header and data cells

--- js/posiciones.js
+++ js/posiciones.js
@@ -30,11 +30,11 @@ async function cargarPosiciones() {
                         <th>#</th>
                         <th>Equipo</th>
                         <th>PJ</th>
-                        <th>PG</th>
-                        <th>PE</th>
-                        <th>PP</th>
-                        <th>GF</th>
-                        <th>GC</th>
+                        <th class="col-px" >PG</th>
+                        <th class="col-px" >PE</th>
+                        <th class="col-px" >PP</th>
+                        <th class="col-px" >GF</th>
+                        <th class="col-px" >GC</th>
                         <th>PTS</th>
 
                     </tr>
@@ -57,12 +57,12 @@ async function cargarPosiciones() {
                         <td>${equipo.equipo}</td>
 
                         <td>${equipo.pj}</td>
-                        <td>${equipo.pg}</td>
-                        <td>${equipo.pe}</td>
-                        <td>${equipo.pp}</td>
+                        <td class="col-px" >${equipo.pg}</td>
+                        <td class="col-px" >${equipo.pe}</td>
+                        <td class="col-px" >${equipo.pp}</td>
 
-                        <td>${equipo.gf}</td>
-                        <td>${equipo.gc}</td>
+                        <td class="col-px" >${equipo.gf}</td>
+                        <td class="col-px" >${equipo.gc}</td>
 
                         <td>${equipo.pts}</td>
 
