ca14871bc8f7a2680c107f7041b7d10bf947ce16
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-15 10:55:06 -0300

    Refactor cargarPosiciones function for partidos data

--- js/posiciones.js
+++ js/posiciones.js
@@ -1,12 +1,110 @@
 async function cargarPosiciones() {
 
-    const response = await fetch("data/posiciones.json");
+    const response =
+        await fetch("data/partidos.json");
 
-    const equipos = await response.json();
+    const partidos =
+        await response.json();
 
-    const tabla = document.getElementById("tablaPosiciones");
+    const equipos = {};
 
-    tabla.innerHTML = `
+    partidos.forEach(partido => {
+
+        if (!equipos[partido.local]) {
+
+            equipos[partido.local] = {
+                nombre: partido.local,
+                pj:0,
+                pg:0,
+                pe:0,
+                pp:0,
+                gf:0,
+                gc:0,
+                pts:0
+            };
+
+        }
+
+        if (!equipos[partido.visitante]) {
+
+            equipos[partido.visitante] = {
+                nombre: partido.visitante,
+                pj:0,
+                pg:0,
+                pe:0,
+                pp:0,
+                gf:0,
+                gc:0,
+                pts:0
+            };
+
+        }
+
+        const local =
+            equipos[partido.local];
+
+        const visitante =
+            equipos[partido.visitante];
+
+        local.pj++;
+        visitante.pj++;
+
+        local.gf += partido.golesLocal;
+        local.gc += partido.golesVisitante;
+
+        visitante.gf += partido.golesVisitante;
+        visitante.gc += partido.golesLocal;
+
+        if (
+            partido.golesLocal >
+            partido.golesVisitante
+        ) {
+
+            local.pg++;
+            local.pts += 3;
+
+            visitante.pp++;
+
+        }
+
+        else if (
+            partido.golesLocal <
+            partido.golesVisitante
+        ) {
+
+            visitante.pg++;
+            visitante.pts += 3;
+
+            local.pp++;
+
+        }
+
+        else {
+
+            local.pe++;
+            visitante.pe++;
+
+            local.pts++;
+            visitante.pts++;
+
+        }
+
+    });
+
+    const tabla =
+        Object.values(equipos);
+
+    tabla.sort((a,b)=>
+        b.pts - a.pts ||
+        (b.gf-b.gc) - (a.gf-a.gc)
+    );
+
+    const html =
+        document.getElementById(
+            "tablaPosiciones"
+        );
+
+    html.innerHTML = `
         <tr>
             <th>#</th>
             <th>Equipo</th>
@@ -14,24 +112,42 @@ async function cargarPosiciones() {
             <th>PG</th>
             <th>PE</th>
             <th>PP</th>
+            <th>GF</th>
+            <th>GC</th>
+            <th>DG</th>
             <th>PTS</th>
         </tr>
     `;
 
-    equipos.forEach(equipo => {
+    tabla.forEach((equipo,index)=>{
 
-        tabla.innerHTML += `
+        html.innerHTML += `
             <tr>
-                <td>${equipo.pos}</td>
-                <td>${equipo.pais}</td>
+
+                <td>${index+1}</td>
+
+                <td>
+                    ${equipo.nombre}
+                </td>
+
                 <td>${equipo.pj}</td>
                 <td>${equipo.pg}</td>
                 <td>${equipo.pe}</td>
                 <td>${equipo.pp}</td>
-                <td>${equipo.pts}</td>
+
+                <td>${equipo.gf}</td>
+                <td>${equipo.gc}</td>
+
+                <td>
+                    ${equipo.gf-equipo.gc}
+                </td>
+
+                <td>
+                    ${equipo.pts}
+                </td>
+
             </tr>
         `;
-
     });
 
 }
