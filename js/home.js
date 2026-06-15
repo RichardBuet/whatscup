a5d069a14269142dfc4fa91be2d8ebac22b78a92
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-15 12:32:26 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,77 @@
+async function cargarHome() {
 
+    const response =
+        await fetch("./data/partidos.json");
+
+    const partidos =
+        await response.json();
+
+    const terminados =
+        partidos.filter(
+            p => p.estado === "FINISHED"
+        );
+
+    const pendientes =
+        partidos.filter(
+            p =>
+                p.estado !== "FINISHED"
+        );
+
+    document.getElementById(
+        "ultimosResultados"
+    ).innerHTML =
+        terminados
+        .slice(-5)
+        .reverse()
+        .map(p => `
+
+            <div class="partido">
+
+                <div class="resultado">
+
+                    ${p.local}
+
+                    <strong>
+
+                        ${p.golesLocal}
+                        -
+                        ${p.golesVisitante}
+
+                    </strong>
+
+                    ${p.visitante}
+
+                </div>
+
+            </div>
+
+        `)
+        .join("");
+
+    document.getElementById(
+        "proximosPartidos"
+    ).innerHTML =
+        pendientes
+        .slice(0,5)
+        .map(p => `
+
+            <div class="partido">
+
+                <div class="resultado">
+
+                    ${p.local}
+
+                    <strong>vs</strong>
+
+                    ${p.visitante}
+
+                </div>
+
+            </div>
+
+        `)
+        .join("");
+
+}
+
+cargarHome();
