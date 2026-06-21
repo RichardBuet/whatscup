6bb845647a2f9fab0675019ccabda1380787b721
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-20 23:32:55 -0300

    Refactor position generation for finished matches

--- scripts/generar-posiciones.js
+++ scripts/generar-posiciones.js
@@ -1,8 +1,111 @@
+import fs from "fs";
+
+const partidos = JSON.parse(
+    fs.readFileSync(
+        "./data/partidos.json",
+        "utf8"
+    )
+);
+
+const grupos = {};
+
+for (const partido of partidos) {
+
+    if (partido.estado !== "FINISHED") {
+        continue;
+    }
+
+    const grupo = partido.grupo || "SIN_GRUPO";
+
+    if (!grupos[grupo]) {
+        grupos[grupo] = {};
+    }
+
+    const tabla = grupos[grupo];
+
+    const local = partido.local;
+    const visitante = partido.visitante;
+
+    if (!tabla[local]) {
+
+        tabla[local] = {
+            equipo: local,
+            grupo: grupo,
+            pts: 0,
+            pj: 0,
+            pg: 0,
+            pe: 0,
+            pp: 0,
+            gf: 0,
+            gc: 0
+        };
+
+    }
+
+    if (!tabla[visitante]) {
+
+        tabla[visitante] = {
+            equipo: visitante,
+            grupo: grupo,
+            pts: 0,
+            pj: 0,
+            pg: 0,
+            pe: 0,
+            pp: 0,
+            gf: 0,
+            gc: 0
+        };
+
+    }
+
+    tabla[local].pj++;
+    tabla[visitante].pj++;
+
+    tabla[local].gf += partido.golesLocal;
+    tabla[local].gc += partido.golesVisitante;
+
+    tabla[visitante].gf += partido.golesVisitante;
+    tabla[visitante].gc += partido.golesLocal;
+
+    if (partido.golesLocal > partido.golesVisitante) {
+
+        tabla[local].pg++;
+        tabla[local].pts += 3;
+
+        tabla[visitante].pp++;
+
+    }
+
+    else if (
+        partido.golesLocal <
+        partido.golesVisitante
+    ) {
+
+        tabla[visitante].pg++;
+        tabla[visitante].pts += 3;
+
+        tabla[local].pp++;
+
+    }
+
+    else {
+
+        tabla[local].pe++;
+        tabla[visitante].pe++;
+
+        tabla[local].pts++;
+        tabla[visitante].pts++;
+
+    }
+
+}
+
 const posiciones = {};
 
-Object.keys(grupos)
-.sort()
-.forEach(grupo => {
+const gruposOrdenados =
+    Object.keys(grupos).sort();
+
+for (const grupo of gruposOrdenados) {
 
     posiciones[grupo] =
         Object.values(grupos[grupo])
@@ -24,4 +127,17 @@ Object.keys(grupos)
 
         });
 
-});
+}
+
+fs.writeFileSync(
+    "./data/posiciones.json",
+    JSON.stringify(
+        posiciones,
+        null,
+        2
+    )
+);
+
+console.log(
+    `${Object.keys(posiciones).length} grupos procesados`
+);
