54e7abb8ca56275590f6bd1485a33421050ac6b5
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-24 15:40:24 -0300

    Update version.js

--- js/version.js
+++ js/version.js
@@ -1,39 +1,24 @@
-document.addEventListener(
-    "DOMContentLoaded",
-    () => {
+fetch(
+    "version.json?v=" +
+    Date.now()
+)
+.then(
+    response =>
+        response.json()
+)
+.then(
+    data => {
 
         const div =
             document.getElementById(
                 "versionApp"
             );
 
-        if (div) {
+        if (!div)
+            return;
 
-            div.innerHTML =
-                "🔥 VERSION FUNCIONANDO 🔥";
-
-            div.style.position =
-                "fixed";
-
-            div.style.top =
-                "10px";
-
-            div.style.right =
-                "10px";
-
-            div.style.background =
-                "red";
-
-            div.style.color =
-                "white";
-
-            div.style.padding =
-                "10px";
-
-            div.style.zIndex =
-                "99999";
-
-        }
+        div.innerHTML =
+            `WhatsCup ${data.version}`;
 
     }
 );
