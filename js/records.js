2251abbc096fe2a538697d09a8e1feff4ff48bce
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-06-30 21:26:29 -0300

    Create records.js

---
+++ js/records.js
@@ -0,0 +1,15 @@
+async function cargarRecords(){
+
+    document.getElementById(
+        "contenidoEstadisticas"
+    ).innerHTML = `
+
+        <h2 class="grupo-titulo">
+            🏆 Récords del Mundial
+        </h2>
+
+        <div id="records"></div>
+
+    `;
+
+}
